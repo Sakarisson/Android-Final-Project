47c19a9b0318bd2a25a0d4fce75a31cf72fc6fec
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-19 20:58:45 +0300

    Implemented basic functionality of BathingSitesView

--- app/src/main/java/se/miun/krsa1201/bathingsites/BathingSitesView.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/BathingSitesView.java
@@ -2,20 +2,41 @@ package se.miun.krsa1201.bathingsites;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public final class BathingSitesView extends View {
+public final class BathingSitesView extends LinearLayout {
     private int _numberOfSites;
+    private TextView _numberOfSitesText;
+    private String _sitesString;
+    private Context _context;
+    private AttributeSet _attrs;
 
     public BathingSitesView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        _context = context;
+        _attrs = attrs;
+        _sitesString = context.getResources().getString(R.string.x_number_of_bathing_sites);
+        init();
     }
 
     private void init() {
-        _numberOfSites = 0;
+        setNumberOfSites(0);
+        inflate(_context, R.layout.bathing_sites_view, this);
+        _numberOfSitesText = findViewById(R.id.bathing_sites_number);
+        setText();
+    }
+
+    private void setText() {
+        String outputText = _numberOfSites + " " + _sitesString;
+        _numberOfSitesText.setText(outputText);
     }
 
     public void setNumberOfSites(int numberOfSites) {
         _numberOfSites = numberOfSites;
+    }
+
+    public int getNumberOfSites() {
+        return _numberOfSites;
     }
 }
