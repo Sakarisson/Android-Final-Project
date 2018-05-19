3f8ffc1b824eb8d2631b754eeba1c1a918e68318
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-19 22:28:24 +0300

    Added XML for bathing sites fragment

---
+++ app/src/main/java/se/miun/krsa1201/bathingsites/BathingSitesFragment.java
@@ -0,0 +1,24 @@
+package se.miun.krsa1201.bathingsites;
+
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public final class BathingSitesFragment extends Fragment {
+
+    public BathingSitesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bathing_sites, container, false);
+    }
+}
+
