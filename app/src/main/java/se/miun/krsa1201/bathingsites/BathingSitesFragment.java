822b886f8c7e16905db54252c1041775ba4d3f55
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-20 10:58:58 +0300

    Implemented bathing sites fragment

--- app/src/main/java/se/miun/krsa1201/bathingsites/BathingSitesFragment.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/BathingSitesFragment.java
@@ -1,8 +1,6 @@
 package se.miun.krsa1201.bathingsites;
 
 import android.support.v4.app.Fragment;
-
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
