05103aae4fdb1979ff264ddc86a7006f4cfef617
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-25 14:38:13 +0300

    Added clear functionality

--- app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
@@ -1,24 +1,47 @@
 package se.miun.krsa1201.bathingsites;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public final class NewBathingSiteActivity extends AppCompatActivity {
+    Fragment newBathingSiteFragment;
+
+    private void init() {
+        newBathingSiteFragment = getSupportFragmentManager().findFragmentById(R.id.new_bathing_site_fragment);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_bathing_site);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        init();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_new_bathing_site, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_clear:
+                ((NewBathingSiteFragment) newBathingSiteFragment).clear();
+                return true;
+
+            case R.id.action_save:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
--- app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteFragment.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteFragment.java
@@ -5,17 +5,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class NewBathingSiteFragment extends Fragment {
+    private EditText _name;
+    private EditText _description;
+    private EditText _address;
+    private EditText _latitude;
+    private EditText _longitude;
+    private RatingBar _grade;
+    private EditText _waterTemp;
+    private EditText _dateForTemp;
+
+    public void clear() {
+        _name.setText("");
+        _description.setText("");
+        _address.setText("");
+        _latitude.setText("");
+        _longitude.setText("");
+        _grade.setRating(0);
+        _waterTemp.setText("");
+        _dateForTemp.setText("");
+    }
+
+    private void init(View inflatedView) {
+        _name = inflatedView.findViewById(R.id.bathing_site_name_input_field);
+        _description = inflatedView.findViewById(R.id.bathing_site_description_input_field);
+        _address = inflatedView.findViewById(R.id.bathing_site_address_input_field);
+        _latitude = inflatedView.findViewById(R.id.bathing_site_latitude_input_field);
+        _longitude = inflatedView.findViewById(R.id.bathing_site_longitude_input_field);
+        _grade = inflatedView.findViewById(R.id.bathing_site_rating_field);
+        _waterTemp = inflatedView.findViewById(R.id.bathing_site_water_temp_input_field);
+        _dateForTemp = inflatedView.findViewById(R.id.bathing_site_date_for_temp_input_field);
+    }
 
     public NewBathingSiteFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_bathing_site, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_new_bathing_site, container, false);
+        if (inflatedView != null) {
+            init(inflatedView);
+        }
+        return inflatedView;
     }
 }
