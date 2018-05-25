398dc7f1f65fd0ef9dfbf484b660ee58a952ac54
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-25 11:49:07 +0300

    Added menu to new bathing site activity

--- app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
@@ -3,6 +3,7 @@ package se.miun.krsa1201.bathingsites;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public final class NewBathingSiteActivity extends AppCompatActivity {
     @Override
@@ -12,4 +13,12 @@ public final class NewBathingSiteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_bathing_site, menu);
+        return true;
+    }
+
 }
