458371e641e49e3fad92ee464ca5c7d93eae4885
Kristian Sakarisson <kristian@sakarisson.com>
2018-05-25 10:00:26 +0300

    Added fragment to new bathing site

--- app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
+++ app/src/main/java/se/miun/krsa1201/bathingsites/NewBathingSiteActivity.java
@@ -1,6 +1,15 @@
 package se.miun.krsa1201.bathingsites;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public final class NewBathingSiteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_bathing_site);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 }
